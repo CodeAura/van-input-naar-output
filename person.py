7260325bef6bf40c856fe4d1ee713f036ffee311
ValidCoder <mikedh.prive@gmail.com>
2021-09-09 09:09:54 +0200

    Gegevens + Screenshot

---
+++ person.py
@@ -0,0 +1,11 @@
+Naam = input('Uw naam:')
+Adres = input('Adres:')
+Postcode = input('Postcode:')
+Woonplaats = input('Woonplaats:')
+
+#Gegevens
+
+print("Naam: " + Naam)
+print("Adres: " + Adres)
+print("Postcode: " + Postcode)
+print("Woonplaats: " + Woonplaats)
